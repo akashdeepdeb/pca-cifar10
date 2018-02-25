afc61beb3cfe2ddc677feb26aa0c10365cd08616
Akashdeep Deb <deb2@illinois.edu>
2018-02-25 14:38:09 -0600

    ani wtf

--- code/main.py
+++ code/main.py
@@ -3,7 +3,7 @@ import pprint
 import numpy as np
 import pandas as pd
 from skimage import io
-import matplotlib
+import matplotlib.pyplot as plt
 from PIL import Image
 
 def unpickle(file):
@@ -62,7 +62,8 @@ def main():
 	    mean.append(mean_arr)
 	    data[i] = data[i].as_matrix()
 
-	#save image
+	#save images
+	'''
 	for i, arr in enumerate(mean):
 		im_arr = np.zeros((32,32,3), 'uint8')
 		im = np.array(arr)
@@ -73,6 +74,39 @@ def main():
 		#io.imsave(results_dir + names[i] + '_mean.png', im)
 		Image.fromarray(im_arr, 'RGB').save(results_dir + names[i] + '_mean.png')
 	return
+	'''
+
+	#Calculate covmat for each category
+	covmat = []
+	for i in range(0,10):
+	    covmat.append(np.cov(data[i],rowvar=False))
+
+	#data_norm calculation
+	data_norm = []
+	for i in range(0,10):    
+	    data_norm.append(data[i].astype(float))
+	    for j in range(0,len(data[i])):
+	        data_norm[i][j] = data_norm[i][j] - mean[i]
+
+	#Find matrix of eigenvectors for covmat
+	eig = []
+	for i in range(0,10):
+	    eig.append(np.linalg.eig(covmat[i]))
+
+	#error calculation
+	error = []
+	for i in range(0,10):
+	    sum_20 = 0
+	    for j in range(0,20):
+	        sum_20 += eig[i][0][j]
+	    error.append(eig[i][0].sum() - sum_20)
+
+	#plotting and displaying the error
+	df_error = pd.DataFrame(error)
+	bar_graph = df_error.plot(kind='bar',title='PCA Error Per Category', figsize=(10,8),fontsize=12, legend=False)
+	bar_graph.set_ylabel("Error",fontsize=12)
+	bar_graph.set_xlabel("Category",fontsize=12)
+	plt.show()
 
 
 if '__main__' == __name__:
